93fccb8cc8d49059b42ec855ecf2841d6a9e9e96
JaredMabus <65479004+JaredMabus@users.noreply.github.com>
2022-12-07 15:21:06 -0600

    added mongoose models for account, workout, and rounds

---
+++ server/models/Round.ts
@@ -0,0 +1,86 @@
+import { Schema, model, Types } from "mongoose";
+import { Workout } from "./index";
+import { WorkoutMethodType } from "./Workout";
+// -- Round Types -- //
+export interface RoundType {
+  _id: Types.ObjectId;
+  accountId: Types.ObjectId;
+  workoutId: Types.ObjectId;
+  date: Date;
+  method: WorkoutMethodType;
+  weight: number;
+  sets: number;
+  reps: number;
+  successSetsReps: boolean;
+}
+
+const RoundSchema = new Schema(
+  {
+    accountId: {
+      type: Schema.Types.ObjectId,
+      ref: "Account",
+      required: true,
+    },
+    workoutId: { type: Schema.Types.ObjectId, ref: "Workout", required: true },
+    date: {
+      type: Date,
+      default: () => Date.now() + 7 * 24 * 60 * 60 * 1000,
+      required: true,
+    },
+    weight: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+    sets: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+    reps: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+    successSetsReps: {
+      type: Boolean,
+      default: true,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+RoundSchema.post("save", async function (doc) {
+  try {
+    await Workout.findByIdAndUpdate(
+      this.workoutId,
+      {
+        $addToSet: { roundId: this._id },
+      },
+      { new: true }
+    );
+  } catch (err) {
+    throw new Error("Could not add RoundId to account");
+  }
+});
+
+RoundSchema.post(new RegExp("delete", "i"), async function (doc) {
+  try {
+    await Workout.findByIdAndUpdate(
+      doc.workoutId,
+      {
+        $pull: { roundId: doc._id },
+      },
+      { new: true }
+    );
+  } catch (err) {
+    throw new Error("Could not delete RoundId to account");
+  }
+});
+
+const Round = model("Round", RoundSchema);
+
+export default Round;
---
+++ server/models/Workout.ts
@@ -0,0 +1,103 @@
+import { Schema, model, Types } from "mongoose";
+import { Account } from "./index";
+
+// -- Workout Types -- //
+export interface WorkoutType {
+  _id?: string | Types.ObjectId;
+  accountId: string | Types.ObjectId;
+  roundId: string[] | Types.ObjectId[] | [];
+  name: string;
+  muscleCategory: WorkoutMuscleCategoryType;
+  muscleGroup: MuscleGroupType;
+  methodSelection: WorkoutMethodType[] | [];
+}
+
+export type WorkoutMuscleCategoryType =
+  | "Upper Body"
+  | "Lower Body"
+  | "Core"
+  | "";
+export type MuscleGroupType =
+  | "Chest"
+  | "Legs"
+  | "Glutes"
+  | "Shoulder"
+  | "Back"
+  | "Front Arm"
+  | "Back Arm"
+  | "Abs";
+export type WorkoutMethodType =
+  | "Barbell"
+  | "Cable"
+  | "Dumbbell"
+  | "Machine"
+  | "";
+
+const WorkoutSchema = new Schema(
+  {
+    accountId: {
+      type: Schema.Types.ObjectId,
+      ref: "Account",
+      required: true,
+    },
+    roundId: [{ type: Schema.Types.ObjectId, ref: "Round", default: [] }],
+    name: {
+      type: String,
+      required: true,
+    },
+    muscleCategory: {
+      type: String,
+      required: true,
+    },
+    muscleGroup: {
+      type: String,
+      required: true,
+    },
+    methodSelection: [
+      {
+        type: String,
+        required: true,
+        enum: ["Barbell", "Cable", "Dumbbell", "Machine"],
+      },
+    ],
+    videoUrl: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+WorkoutSchema.post("save", async function (doc) {
+  try {
+    await Account.findByIdAndUpdate(
+      this.accountId,
+      {
+        $addToSet: { workoutId: this._id },
+      },
+      { new: true }
+    );
+  } catch (err) {
+    throw new Error("Could not add workoutId to account");
+  }
+});
+
+WorkoutSchema.post(new RegExp("delete", "i"), async function (doc) {
+  try {
+    await Account.findByIdAndUpdate(
+      doc.accountId,
+      {
+        $pull: { workoutId: doc._id },
+      },
+      { new: true }
+    );
+  } catch (err) {
+    throw new Error("Could not delete workoutId to account");
+  }
+});
+
+const Workout = model("Workout", WorkoutSchema);
+
+export default Workout;
