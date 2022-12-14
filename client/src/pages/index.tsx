1eabb1468d4e959f1ec5617295b43c3b6819aa3f
JaredMabus <65479004+JaredMabus@users.noreply.github.com>
2022-12-14 12:17:55 -0600

    pages dir, index and Home Page

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Home from "./Home";
+import Account from "./Account";
+import Login from "./Login";
+import SignUp from "./SignUp";
+import Dashboard from "./Dashboard";
+// import Workouts from "./Workouts/Workouts";
+// import Plan from "./Plan/Plan";
+
+export {
+  Home,
+  Account,
+  Login,
+  SignUp,
+  Dashboard,
+  // Workouts,
+  // Plan
+};
