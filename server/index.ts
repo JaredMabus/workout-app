f35e6e04c42c019f965ba456f501c16ecc74374f
JaredMabus <65479004+JaredMabus@users.noreply.github.com>
2023-04-22 20:17:15 -0500

    added elastic IP to CORS

--- server/index.ts
+++ server/index.ts
@@ -26,7 +26,11 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:8080"],
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:8080",
+      "http://3.12.238.39",
+    ],
     credentials: true,
   })
 );
